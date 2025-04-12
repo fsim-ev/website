0e2cd0454c0e377307a0ea440020135a114548cc
DestinyofYeet <ole@ole.blue>
2025-04-13 00:10:36 +0200

    feat(meta/nix): Can now build site with 'nix build'

--- flake.nix
+++ flake.nix
@@ -1,13 +1,22 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    papermod-theme = {
+      url = "github:adityatelange/hugo-PaperMod";
+      flake = false;
+    };
+  };
 
-  outputs = { self, nixpkgs, }:
+  outputs = { self, nixpkgs, ... }@inputs:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
 
     in {
       devShells.x86_64-linux.default =
         pkgs.mkShell { nativeBuildInputs = with pkgs; [ hugo ]; };
+
+      packages.x86_64-linux.default =
+        pkgs.callPackage ./pkg.nix { inherit inputs; };
     };
 }
---
+++ pkg.nix
@@ -0,0 +1,28 @@
+{ pkgs, inputs, ... }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "fachschaft-IM-website";
+  version = "1.0";
+
+  src = ./.;
+
+  buildInputs = with pkgs; [ hugo ];
+
+  configurePhase = ''
+    echo src is $src
+    mkdir -p themes/PaperMod
+
+    cp -r ${inputs.papermod-theme}/* themes/PaperMod
+  '';
+
+  buildPhase = ''
+    hugo build
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -r public/* $out
+    runHook postInstall
+  '';
+}
